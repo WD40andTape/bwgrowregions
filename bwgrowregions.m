4c3e5405729f535ddede34eb79bf72107be7764e
WD40andTape <georgehabrahams@gmail.com>
2023-12-25 01:03:03 +0000

    Optimize finding neighbouring pixels and assessing non-unique neighbours - Use linear indices rather than subscripts to find neighbouring pixels, avoiding a call to sub2ind. - Remove NaN (do-not-label) neighbours before selecting the minimum distance for non-unique neighbours, speeding up the costly call to unique.

--- bwgrowregions.m
+++ bwgrowregions.m
@@ -1,5 +1,5 @@
 function I = bwgrowregions( I, method )
-%BWGROWREGIONS
+%BWGROWREGIONS Multi-class, distance-based segmentation of binary images.
 %   - I is a 2D image or a 3D volume, where:
 %   -- 0 represents unlabelled pixels
 %   -- NaN represents pixels not to label
@@ -10,36 +10,50 @@ function I = bwgrowregions( I, method )
         method { mustBeMember( method, { 'chessboard', 'cityblock', ...
             'quasi-euclidean' } ) } = 'quasi-euclidean'
     end
-    
-    % Base defines the subscript offsets to each neighbour. In 2D, the 
-    % connectivity is 8, and in 3D, 26. Precomputed for efficiency, but 
-    % calculated as such:
-    %
-    % conn = 4, 6, 8, 18, or 26
-    % connMatrix = images.internal.getBinaryConnectivityMatrix(conn);
-    % connMatrix(2,2,2) = false; % Set the central index to 0
-    % [ baseI, baseJ, baseK ] = ndgrid( -1 : 1 );
-    % if ismatrix( I )
-    %     baseK(:) = 0;
-    % end
-    % base = [ baseI(connMatrix) baseJ(connMatrix) baseK(connMatrix) ];
-    %
+
+    % Pad the image so that the neighbours of all pixel are within the 
+    % matrix bounds.
     if ismatrix( I )
-        base = [-1,-1,0;0,-1,0;1,-1,0;-1,0,0;1,0,0;-1,1,0;0,1,0;1,1,0];
-    else
-        base = [-1,-1,-1;0,-1,-1;1,-1,-1;-1,0,-1;0,0,-1;1,0,-1; ...
-                -1,1,-1;0,1,-1;1,1,-1;-1,-1,0;0,-1,0;1,-1,0;-1,0,0; ...
-                1,0,0;-1,1,0;0,1,0;1,1,0;-1,-1,1;0,-1,1;1,-1,1; ...
-                -1,0,1;0,0,1;1,0,1;-1,1,1;0,1,1;1,1,1];
+        I = padarray( I, [1 1], NaN );
+    else % 3D (volume).
+        I = padarray( I, [1 1 1], NaN );
+    end
+    [ h, w, d ] = size( I );
+    
+    % Base defines the offsets to each neighbour in linear indices. In 2D, 
+    % the pixel connectivity is 8, and in 3D, 26. baseDistance defines the 
+    % distance to each neighbour according to the chosen distance metric. 
+    % Precomputed for efficiency. Using linear indices rather than 
+    % subscripts avoids a call to sub2ind.
+    if d == 1 % 2D (matrix).
+        base = [ -h-1, -h, -h+1, -1, +1, +h-1, +h, +h+1 ]';
+        if strncmp( method, 'chessboard', 2 )
+            baseDistance = [ 1, 1, 1, 1, 1, 1, 1, 1 ]';
+        elseif strncmp( method, 'cityblock', 2 )
+            baseDistance = [ 2, 1, 2, 1, 1, 2, 1, 2 ]';
+        else % 'quasi-euclidean'
+            baseDistance = [ sqrt(2), 1, sqrt(2), 1, 1, ...
+                sqrt(2), 1, sqrt(2) ]';
+        end
+    else % 3D (volume).
+        base = [ -h*w-h-1, -h*w-h, -h*w-h+1, -h*w-1, -h*w, -h*w+1, ...
+            -h*w+h-1, -h*w+h, -h*w+h+1, -h-1, -h, -h+1, -1, +1, +h-1, ...
+            +h, +h+1, +h*w-h-1, +h*w-h, +h*w-h+1, +h*w-1, +h*w, +h*w+1, ...
+            +h*w+h-1, +h*w+h, +h*w+h+1 ]';
+        if strncmp( method, 'chessboard', 2 )
+            baseDistance = [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, ...
+                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]';
+        elseif strncmp( method, 'cityblock', 2 )
+            baseDistance = [ 3, 2, 3, 2, 1, 2, 3, 2, 3, 2, 1, 2, 1, ...
+                1, 2, 1, 2, 3, 2, 3, 2, 1, 2, 3, 2, 3 ]';
+        else % 'quasi-euclidean'
+            baseDistance = [ sqrt(3), sqrt(2), sqrt(3), sqrt(2), 1, ...
+                sqrt(2), sqrt(3), sqrt(2), sqrt(3), sqrt(2), 1, ...
+                sqrt(2), 1, 1, sqrt(2), 1, sqrt(2), sqrt(3), sqrt(2), ...
+                sqrt(3), sqrt(2), 1, sqrt(2), sqrt(3), sqrt(2), sqrt(3) ]';
+        end
     end
     connectivity = size( base, 1 );
-    if strcmp( method, 'chessboard' )
-        baseDistance = ones( connectivity, 1 );
-    elseif strcmp( method, 'cityblock' )
-        baseDistance = sum( abs( base ), 2 );
-    else % 'quasi-euclidean'
-        baseDistance = vecnorm( base, 2, 2 );
-    end
 
     sourceIndices = find( ~isnan( I ) & I ~= 0 );
     sourceValues = I(sourceIndices);
@@ -51,42 +65,40 @@ function I = bwgrowregions( I, method )
     % experiments, this was never faster than bwgrowregions, and 
     % substantially slower when many labels are used.
     seedLabels = unique( sourceValues );
-    numSeeds = numel( seedLabels );
-    if numSeeds == 1
+    numLabels = numel( seedLabels );
+    if numLabels == 1
         bw = ~isnan( I );
         mask = I == seedLabels;
         geodesicDist = bwdistgeodesic( bw, mask, method );
         I(~isnan( geodesicDist )) = seedLabels;
         return % EARLY RETURN.
     end
-        
-    sz = size( I, [1 2 3] ); % Specify dims to handle 2D case.
-    geodesicDist = inf( sz );
+    
+    geodesicDist = inf( [ h w d ] );
     geodesicDist(sourceIndices) = 0;
-    [ I1, I2, I3 ] = ind2sub( sz, sourceIndices );
-    source = [ I1, I2, I3 ];
 
     % Dilate repeatedly, until no more pixels can be reached.
     while true
 
         % Add base indices to source indices to find all neighbours.
-        neighbours = repelem( source, connectivity, 1 ) + ...
-            repmat( base, size(source,1), 1 );
-        distToNeighbours = repmat( baseDistance, size(source,1), 1 );
-        geodesicDistAtSource = geodesicDist(  repelem( sourceIndices, connectivity, 1 ) );
+        numSources = numel( sourceIndices );
+        sourceIndices = repelem( sourceIndices, connectivity, 1 );
+        neighbourIndices = sourceIndices + repmat( base, numSources, 1 );
+        distToNeighbours = repmat( baseDistance, numSources, 1 );
+        geodesicDistAtSource = geodesicDist( sourceIndices );
         geodesicDistAtNeighbours = geodesicDistAtSource + distToNeighbours;
         sourceValues = repelem( sourceValues, connectivity, 1 );
 
-        % Remove out of image neighbours.
-        valid = all( neighbours > 0 & neighbours <= sz, 2);
-        neighbours = neighbours(valid,:);
-        geodesicDistAtNeighbours = geodesicDistAtNeighbours(valid,:);
-        sourceValues = sourceValues(valid,:);
-        % Remove do-not-label (NaN) pixels and those with a greater 
-        % distance than the alternatives.
-        neighboursIndices = sub2ind( sz, ...
-            neighbours(:,1), neighbours(:,2), neighbours(:,3) );
-        [ ~, grName, grNum ] = unique( neighboursIndices );
+        % Stop assessing trivial cases: do-not-label (NaN) pixels and those 
+        % with a greater distance than that of the pre-existing label.
+        isKeep = ~isnan( I(neighbourIndices) ) & ...
+            geodesicDistAtNeighbours < geodesicDist( neighbourIndices );
+        neighbourIndices = neighbourIndices(isKeep,:);
+        geodesicDistAtNeighbours = geodesicDistAtNeighbours(isKeep,:);
+        sourceValues = sourceValues(isKeep,:);
+        % For non-unique neighbours, select the neighbour with the minimum
+        % distance.
+        [ ~, grName, grNum ] = unique( neighbourIndices );
         grValue = inf( size( grName ) );
         grIdx = zeros( size( grName ) );
         for i = 1 : length( geodesicDistAtNeighbours )
@@ -95,28 +107,21 @@ function I = bwgrowregions( I, method )
                 grIdx( grNum(i) ) = i;
             end
         end
-        isOverwrite = false( size( neighboursIndices ) );
+        isOverwrite = false( size( neighbourIndices ) );
         isOverwrite( grIdx ) = true;
-        preGeodesicDistAtNeighbours = geodesicDist( neighboursIndices );
-        isOverwrite = isOverwrite & ...
-            geodesicDistAtNeighbours < preGeodesicDistAtNeighbours;
-        valid = ~isnan( I(neighboursIndices) ); % Neighbours are traversable (nonNan)
-        valid = valid & isOverwrite; % Neighbours are min distance.
-        neighbours = neighbours(valid,:);
-        neighboursIndices = neighboursIndices(valid,:);
-        geodesicDistAtNeighbours = geodesicDistAtNeighbours(valid,:);
-        sourceValues = sourceValues(valid,:);
+        neighbourIndices = neighbourIndices(isOverwrite,:);
+        geodesicDistAtNeighbours = geodesicDistAtNeighbours(isOverwrite,:);
+        sourceValues = sourceValues(isOverwrite,:);
 
         % Dilate with source values.
-        I(neighboursIndices) = sourceValues;
-        geodesicDist(neighboursIndices) = geodesicDistAtNeighbours;
+        I(neighbourIndices) = sourceValues;
+        geodesicDist(neighbourIndices) = geodesicDistAtNeighbours;
 
         % Use newly labeled pixels as input for next iteration.
-        source = neighbours;
-        sourceIndices = neighboursIndices;
+        sourceIndices = neighbourIndices;
 
         % Exit loop if the region has stopped growing.
-        if isempty( source )
+        if isempty( sourceIndices )
             if any( I == 0 )
                 warning( [ 'Some pixels were unreachable from the seed ' ...
                     'labels and were left unlabelled.' ] )
@@ -124,6 +129,13 @@ function I = bwgrowregions( I, method )
             break
         end
 
+    end
+
+    % Remove padding.
+    if d == 1 % 2D (matrix).
+        I = I( 2:end-1, 2:end-1 );
+    else % 3D (volume).
+        I = I( 2:end-1, 2:end-1, 2:end-1 );
     end
     
 end
